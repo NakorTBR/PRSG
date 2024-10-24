fe00684c45d121c3beff8151945c8c9e99c63ad6
NakorTBR <nakor_@hotmail.com>
2024-10-24 14:22:24 -0400

    * Added functionality to split nodes on links or images (MD formatted string nodes).  * Added some inline documentation.  * More tests.  I know.  Surprising.

--- src/nodehandlers.py
+++ src/nodehandlers.py
@@ -1,47 +1,6 @@
 from textnode import TextType, TextNode
 import re
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    retval = []
-    # print(f"OLD: {old_nodes}")
-
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT.value:
-            retval.append(node)
-            continue
-
-        modified_nodes = []
-        lines = node.text.split(delimiter)
-        if len(lines) % 2 == 0:
-            raise ValueError("Bad markdown!  Element was not closed.")
-        
-        for i in range(len(lines)):
-            if lines[i] == "":
-                continue
-            if i % 2 == 0:
-                modified_nodes.append(TextNode(text=lines[i], text_type=TextType.TEXT))
-            else:
-                modified_nodes.append(TextNode(text=lines[i], text_type=text_type))
-        
-        retval.extend(modified_nodes)
-    return retval
-
-def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT.value:
-        return LeafNode(tag=None, value=text_node.text)
-    if text_node.text_type == TextType.BOLD.value:
-        return LeafNode(tag="b", value=text_node.text)
-    if text_node.text_type == TextType.ITALIC.value:
-        return LeafNode(tag="i", value=text_node.text)
-    if text_node.text_type == TextType.CODE.value:
-        return LeafNode(tag="code", value=text_node.text)
-    if text_node.text_type == TextType.LINK.value:
-        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
-    if text_node.text_type == TextType.IMAGE.value:
-        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
-        
-    raise ValueError(f"Invalid text type: {text_node.text_type}")
-
 def extract_markdown_images(text):
     """Takes a markdown string and returns a list of tuples with an image URL and alt text.
 
@@ -79,4 +38,159 @@ def extract_markdown_links(text):
     reg = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     link_tuples = re.findall(reg, text)
     return link_tuples
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    """Splits strings into sections by the delimiter.  Sets the TextType for the sections 
+    based on their section / the passed text_type.
+
+    Parameters
+    ----------
+    old_nodes : list[str]
+        A list of strings containing old nodes of text that need to be processed.
+    delimiter: str
+        The delimiter is the string to split the nodes on.
+    text_type: TextType
+        The text type to set the split node type to.  Normal text will be set as TextType.TEXT, 
+        but the node being sliced off with the delimiter will be set to this type.
+    
+    Returns
+    -------
+    list[tuple]
+        A list of tuples.  Each tuple contains both a URL and the alt text for the link.
+    """
+    retval = []
+    # print(f"OLD: {old_nodes}")
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT.value:
+            retval.append(node)
+            continue
+
+        modified_nodes = []
+        lines = node.text.split(delimiter)
+        if len(lines) % 2 == 0:
+            raise ValueError("Bad markdown!  Element was not closed.")
+        
+        for i in range(len(lines)):
+            if lines[i] == "":
+                continue
+            if i % 2 == 0:
+                modified_nodes.append(TextNode(text=lines[i], text_type=TextType.TEXT))
+            else:
+                modified_nodes.append(TextNode(text=lines[i], text_type=text_type))
+        
+        retval.extend(modified_nodes)
+    return retval
+
+def split_nodes_image(old_nodes):
+    """Takes a list of MD formatted strings and returns TextNode objects (images) populated with the string elements.
+
+    Parameters
+    ----------
+    old_nodes : list[str]
+        A list containing one or more MD formatted strings.
+    
+    Returns
+    -------
+    list[TextNode]
+        A list of TextNode objects.  Each node is either an image link or a basic text node.
+    """
+
+    retval = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT.value:
+            retval.append(node)
+            continue
+
+        # Hold modified nodes when they are ready
+        modified_nodes = []
+        # After stripping a chunk off a string, store the remainder here.
+        remainder = ""
+        links_initial = extract_markdown_images(node.text)
+        for link in links_initial:
+            image_alt = link[0]
+            image_link = link[1]
+            lines = ""
+            if remainder == "":
+                lines = node.text.split(f"![{image_alt}]({image_link})", 1)
+            else:
+                lines = remainder.split(f"![{image_alt}]({image_link})", 1)
+            if len(lines) > 1:
+                remainder = lines[1]
+            modified_nodes.append(TextNode(text=lines[0], text_type=TextType.TEXT))
+            modified_nodes.append(TextNode(text=link[0], text_type=TextType.IMAGE, url=link[1]))
+        
+        if remainder != "":
+            modified_nodes.append(TextNode(text=remainder, text_type=TextType.TEXT))
+        
+        retval.extend(modified_nodes)
+    
+    return retval
+        
+
+
+def split_nodes_link(old_nodes):
+    """Takes a list of MD formatted strings and returns TextNode objects (links) populated with the string elements.
+
+    Parameters
+    ----------
+    old_nodes : list[str]
+        A list containing one or more MD formatted strings.
+    
+    Returns
+    -------
+    list[TextNode]
+        A list of TextNode objects.  Each node is either a link or a basic text node.
+    """
+
+    retval = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT.value:
+            retval.append(node)
+            continue
+
+        # Hold modified nodes when they are ready
+        modified_nodes = []
+        # After stripping a chunk off a string, store the remainder here.
+        remainder = ""
+        links_initial = extract_markdown_links(node.text)
+        for link in links_initial:
+            found_alt = link[0]
+            found_link = link[1]
+            lines = ""
+            if remainder == "":
+                lines = node.text.split(f"[{found_alt}]({found_link})", 1)
+            else:
+                lines = remainder.split(f"[{found_alt}]({found_link})", 1)
+            if len(lines) > 1:
+                remainder = lines[1]
+            modified_nodes.append(TextNode(text=lines[0], text_type=TextType.TEXT))
+            modified_nodes.append(TextNode(text=link[0], text_type=TextType.LINK, url=link[1]))
+        
+        if remainder != "":
+            modified_nodes.append(TextNode(text=remainder, text_type=TextType.TEXT))
+        
+        retval.extend(modified_nodes)
+    
+    return retval
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT.value:
+        return LeafNode(tag=None, value=text_node.text)
+    if text_node.text_type == TextType.BOLD.value:
+        return LeafNode(tag="b", value=text_node.text)
+    if text_node.text_type == TextType.ITALIC.value:
+        return LeafNode(tag="i", value=text_node.text)
+    if text_node.text_type == TextType.CODE.value:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_node.text_type == TextType.LINK.value:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE.value:
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
 
--- src/test_splitnodesdelimiter.py
+++ src/test_splitnodesdelimiter.py
@@ -1,5 +1,5 @@
 import unittest
-from nodehandlers import split_nodes_delimiter
+from nodehandlers import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 from textnode import TextNode, TextType
 
@@ -84,8 +84,142 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             new_nodes,
         )
 
-    
 
+class TestSplitNodes(unittest.TestCase):
+    def test_split_nodes_images(self):
+        node = TextNode(
+        "This is text with an image ![a can in a bucket](url/of/image.jpg) and ![neither image is real](url/of/image.jpg)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode(text="This is text with an image ", text_type=TextType.TEXT),
+                TextNode(text="a can in a bucket", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" and ", text_type=TextType.TEXT),
+                TextNode(text="neither image is real", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+            ],
+            new_nodes,
+        )
 
+    def test_split_nodes_images_three(self):
+        node = TextNode(
+        "We are going to have ![a picture](url/of/image.jpg) three ![no image is real](url/of/image.jpg)"
+         " images here ![cool pic](http://www.atotallyrealwebsite.com)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode(text="We are going to have ", text_type=TextType.TEXT),
+                TextNode(text="a picture", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" three ", text_type=TextType.TEXT),
+                TextNode(text="no image is real", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" images here ", text_type=TextType.TEXT),
+                TextNode(text="cool pic", text_type=TextType.IMAGE, url="http://www.atotallyrealwebsite.com"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_images_three_with_post_text(self):
+        node = TextNode(
+        "We are going to have ![a picture](url/of/image.jpg) three ![no image is real](url/of/image.jpg)"
+         " images here ![cool pic](http://www.atotallyrealwebsite.com) and this text comes after.",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode(text="We are going to have ", text_type=TextType.TEXT),
+                TextNode(text="a picture", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" three ", text_type=TextType.TEXT),
+                TextNode(text="no image is real", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" images here ", text_type=TextType.TEXT),
+                TextNode(text="cool pic", text_type=TextType.IMAGE, url="http://www.atotallyrealwebsite.com"),
+                TextNode(text=" and this text comes after.", text_type=TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_images_three_with_post_text_confusion_check(self):
+        node = TextNode(
+        "We are going to have ![a picture](url/of/image.jpg) three ![no image is real](url/of/image.jpg)"
+         " images here ![cool pic](http://www.atotallyrealwebsite.com) and this text comes after! [Be "
+         "careful with links] because they are important! (but whatever).",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode(text="We are going to have ", text_type=TextType.TEXT),
+                TextNode(text="a picture", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" three ", text_type=TextType.TEXT),
+                TextNode(text="no image is real", text_type=TextType.IMAGE, url="url/of/image.jpg"),
+                TextNode(text=" images here ", text_type=TextType.TEXT),
+                TextNode(text="cool pic", text_type=TextType.IMAGE, url="http://www.atotallyrealwebsite.com"),
+                TextNode(text=" and this text comes after! [Be careful with links] because they are important! "
+                         "(but whatever).", text_type=TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_links(self):
+        node = TextNode(
+        "This is text with a link [I am a link](https://www.boot.dev) and [Google](http://www.google.com)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode(text="This is text with a link ", text_type=TextType.TEXT),
+                TextNode(text="I am a link", text_type=TextType.LINK, url="https://www.boot.dev"),
+                TextNode(text=" and ", text_type=TextType.TEXT),
+                TextNode(text="Google", text_type=TextType.LINK, url="http://www.google.com"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_links_complex(self):
+        node = TextNode(
+        "This is text with a link [I am a link & $t00p1D stuff](https://www.boot.dev?user=FarkleDarkle|doy=duhdoy) "
+        "and [Google](http://www.google.com?h4x=d0g&prox=vangard) with text at the end.",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode(text="This is text with a link ", text_type=TextType.TEXT),
+                TextNode(text="I am a link & $t00p1D stuff", text_type=TextType.LINK, 
+                         url="https://www.boot.dev?user=FarkleDarkle|doy=duhdoy"),
+                TextNode(text=" and ", text_type=TextType.TEXT),
+                TextNode(text="Google", text_type=TextType.LINK, 
+                         url="http://www.google.com?h4x=d0g&prox=vangard"),
+                TextNode(text=" with text at the end.", text_type=TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_links_confusion_check(self):
+        node = TextNode(
+        "This is text with a link [I am a link & $t00p1D stuff](https://www.boot.dev?user=FarkleDarkle|doy=duhdoy) "
+        "and [Google](http://www.google.com?h4x=d0g&prox=vangard) with text at the end.  I am an idiot and like [to "
+        "put things in] boxes to confuse [the code]  (but not in a mean way).",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode(text="This is text with a link ", text_type=TextType.TEXT),
+                TextNode(text="I am a link & $t00p1D stuff", text_type=TextType.LINK, 
+                         url="https://www.boot.dev?user=FarkleDarkle|doy=duhdoy"),
+                TextNode(text=" and ", text_type=TextType.TEXT),
+                TextNode(text="Google", text_type=TextType.LINK, url="http://www.google.com?h4x=d0g&prox=vangard"),
+                TextNode(text=" with text at the end.  I am an idiot and like [to put things in] boxes to confuse "
+                         "[the code]  (but not in a mean way).", text_type=TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+        
 if __name__ == "__main__":
     unittest.main()
